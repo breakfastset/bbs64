1b9adbccc95751f9a2d5c7804048b86748900f18
breakfastset <34071068+breakfastset@users.noreply.github.com>
2024-11-22 18:40:46 +0800

    Add lesson 01

---
+++ lesson01/hello_world.py
@@ -0,0 +1,31 @@
+print("hello world")
+print("bye")
+print("tatatata")    # default end="\n"  new line
+
+print("this line ", end="")   # change the end to empty string ""
+print("that line should be together")
+
+print('This way')    # use single quotes   -> indicate the text within
+print("That way")    # use double quotes
+
+# She says "This is great!"
+print('She says "This is great!"')
+# I'll be around
+print("I'll be around")
+
+# They said "We'll be glad to help"
+print('They said "We\'ll be glad to help"')
+# \' escapes ' to indicate it's part of the string
+
+print("They said \"We'll be glad to help\"")
+# \" escapes " to indicate it's part of the string
+
+# this line that line should be together
+
+# print("tata")
+
+# 1. Case sensitive
+# 2. Indentation/space sensitive
+
+# hash tag means comments
+# not interpreted by Python
